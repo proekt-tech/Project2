ba70d9f483bb78861bb7643f1ca3ab7986d8f534
Mr-MistySmile <grisha.lalayan@mail.ru>
2020-02-12 15:37:04 +0400

    5

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -1,15 +1,9 @@
-#include<iostream>
+п»ї#include<iostream>
 #include<clocale>
 using namespace std;
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	int a, b, c, d;
-	cout << "Введите 3 слагаемых " << endl;
-	cin >> a >> b >> c;
-	d = a + b + c;
-	cout << "Сумма: " <<d<<endl;
-	system("Pause");
 
 }

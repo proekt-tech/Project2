5add6fab3ad6093eb322073afa5382e0feaaf78c
Mr-MistySmile <grisha.lalayan@mail.ru>
2020-02-12 15:09:51 +0400

    Описываю что сделал тут ещё раз

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -5,5 +5,11 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "rus");
+	int a, b, c, d;
+	cout << "¬ведите 3 слагаемых " << endl;
+	cin >> a >> b >> c;
+	d = a + b + c;
+	cout << "—умма: " <<d<<endl;
+	system("Pause");
 
 }
